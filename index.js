8dd0d2c179a6c2e4461c6a005a9ca99e5ae79c53
Vujadin Dagovic <vujke456@gmail.com>
2022-12-18 22:51:07 +0100

    working on the logic of the app

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 import dogs from "./data.js";
 
+let hasBeenDisliked = false;
+
 class Dog {
   constructor(data) {
     Object.assign(this, data);
   }
-  getDog() {
-    const { name, avatar, age, bio } = this;
+  getLike() {
     return `
+    <img class="like-badge" src="/images/badge-like.png" alt="">
+    `;
+  }
+  getNope() {
+    return `
+    <img class="like-badge" src="/images/badge-nope.png" alt="">
+    `;
+  }
+
+  getEndMesagge() {}
+  getDog() {
+    const { name, avatar, age, bio, hasBeenLiked, hasBeenSwiped } = this;
+    return `
+    ${hasBeenLiked ? this.getLike() : hasBeenDisliked ? this.getNope() : ""}
     <img class="dog-img" src="${avatar}" alt="">
     <div class="description">
       <h2 class="dog-name">${name}, ${age}</h2>
@@ -15,11 +30,42 @@ class Dog {
   }
 }
 
-function render() {
-  return (document.getElementById("main").innerHTML = dog1.getDog());
+document.getElementById("like").addEventListener("click", function () {
+  dog.hasBeenLiked = true;
+  dog.hasBeenSwiped = true;
+  if (dog.hasBeenSwiped) {
+    setTimeout(() => {
+      isClicked = true;
+      dog = new Dog(getNewDog());
+      render();
+      isClicked = false;
+    }, 2000);
+  }
+  render();
+});
+
+document.getElementById("cross").addEventListener("click", function () {
+  hasBeenDisliked = true;
+  dog.hasBeenLiked = false;
+  dog.hasBeenSwiped = true;
+  if (dog.hasBeenSwiped) {
+    setTimeout(() => {
+      dog = new Dog(getNewDog());
+      dog.hasBeenDisliked = false;
+      hasBeenDisliked = false;
+      render();
+    }, 2000);
+  }
+  render();
+});
+
+function getNewDog() {
+  return dogs.length > 0 ? dogs.shift() : {};
 }
 
-const dog1 = new Dog(dogs[0]);
-render();
+function render() {
+  return (document.getElementById("main").innerHTML = dog.getDog());
+}
 
-/* <img class="like-badge" src="/images/badge-like.png" alt=""> */
+let dog = new Dog(getNewDog());
+render();
